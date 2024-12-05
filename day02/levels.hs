44cfa2c39500e16c22dc38aa96e2035665e5bb7d
ACR1209 <andrescoronel1209@gmail.com>
2024-12-05 09:42:50 -0500

    Day 02: Second puzzle done

--- day02/levels.hs
+++ day02/levels.hs
@@ -14,8 +14,27 @@ parseLine line =
 isValidReport :: [Int] -> Bool
 isValidReport report = allDecrease || allIncrease
     where
-        allDecrease = all (\(x, y) -> x >= y && abs (x - y) <= 3 && abs (x - y) >= 1) (zip report (tail report))
-        allIncrease = all (\(x, y) -> x <= y && abs (x - y) <= 3 && abs (x - y) >= 1) (zip report (tail report))
+        pairedLevels = zip report (tail report)
+        allDecrease = all isDecreaseWithinRange pairedLevels
+        allIncrease = all isIncreaseWithinRange pairedLevels
+
+isDecreaseWithinRange :: (Int, Int) -> Bool
+isDecreaseWithinRange (x, y) = x >= y && abs (x - y) <= 3 && abs (x - y) >= 1
+
+isIncreaseWithinRange :: (Int, Int) -> Bool
+isIncreaseWithinRange (x, y) = x <= y && abs (x - y) <= 3 && abs (x - y) >= 1
+
+isValidReportWithProblemDampener :: [Int] -> Bool
+isValidReportWithProblemDampener report = isValidReport report || ((length $ getInvalidLevels report) <= ((length report) - 1)) 
+
+getInvalidLevels :: [Int] -> [Int]
+getInvalidLevels report = [report !! i | i <- [0..(length report - 1)], not (isValidReport (removeAtIndex i report))]
+
+removeAtIndex :: Int -> [a] -> [a]
+removeAtIndex i xs = let (ys, zs) = splitAt i xs in ys ++ (tail zs)
+
+numberOfSafeReports :: [Bool] -> Int
+numberOfSafeReports reportsValidity = length $ filter (id) reportsValidity
 
 main :: IO ()
 main = do
@@ -24,7 +43,16 @@ main = do
 
     reports <- readReports
     let validityOfReports = map isValidReport reports
-    let safeReports = length $ filter (id) validityOfReports 
+    let safeReports = numberOfSafeReports validityOfReports 
 
     putStr "Number of safe reports: "
     putStrLn $ show safeReports
+
+    putStrLn "\n\n\nDay 2: Second puzzle"
+    putStrLn "--------------------"
+
+    let validityOfReportsWithDampener = map isValidReportWithProblemDampener reports
+    let safeReportsDampener = numberOfSafeReports validityOfReportsWithDampener
+
+    putStr "Number of safe reports with dampener: "
+    putStrLn $ show safeReportsDampener 
